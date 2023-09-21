c4c28eb5c73f01f463f6b034ac0c0ced28cb8f97
Aaron Joshua Dela Cruz <aaronjoshua.dlacruz@gmail.com>
2023-09-21 11:00:05 +0800

    update the portfolio image
    resize the reso of christmascountdown

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -11,7 +11,7 @@ export default {
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
           github: 'https://github.com/aaronjoshuadelacruz/portfolio.git',
-          demo: 'https://aaronjoshua.netlify.app/'
+          demo: 'https://aaronjoshuadev.netlify.app/'
         },
         {
           id: 2,
